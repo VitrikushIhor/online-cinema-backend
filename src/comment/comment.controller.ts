8f9bf809926a87668fb32a750d78cb69e5366f07
Ihor <vitrikushihor0101@gmail.com>
2023-04-29 14:17:46 +0300

    add delete commit

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,15 @@
-import {Body, Controller, Get, HttpCode, Param, UsePipes, ValidationPipe,Post} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  UsePipes,
+  ValidationPipe,
+  Post,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import {Types} from 'mongoose';
 import {IdValidationPipe} from '../pipes/id.validation.pipe';
@@ -17,18 +28,26 @@ export class CommentController {
 
   @Get('by-movie/:movieId')
   async getCommentByVideoId(
-     @Param('movieId', IdValidationPipe) movieId: Types.ObjectId
+     @Param('movieId', IdValidationPipe) movieId: string
   ) {
     return this.commentService.byMovieId(movieId)
   }
+
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post()
   @Auth()
   async createComment(
-     @User('_id') _id: Types.ObjectId,
+     @User('_id') _id: string,
      @Body() dto: CommentDto
   ) {
     return this.commentService.create(_id, dto)
   }
+  @Delete(':id')
+  @Auth()
+  async delete(@Param('id') id: string) {
+    const deletedDoc = await this.commentService.delete(id)
+    if (!deletedDoc) throw new NotFoundException('Comment not found')
+  }
+
 }
